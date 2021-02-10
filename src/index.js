8cfc20cb2b5e11ad55d7c14281ddbfd415bc73d6
Elvis Landi <elandi13@yahoo.com>
2021-02-10 11:30:55 -0500

    So Close

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 // write your code here
 
-const fetchUrl = "http://localhost:3000/spiceblends/1"
+const fetchUrl = "http://localhost:3000/spiceblends"
 
-const spiceMenu = document.querySelector('#spice-blend-detail')
-
+const spiceMenu = document.getElementById('spice-images')
+const spiceDetails = document.getElementById('spice-blend-detail')
+spiceMenu.addEventListener('click', getSpiceInfo)
 document.addEventListener('load', getSpice)
-// console.log(spiceMenu)
+
 
 function getSpice(){
     fetch(fetchUrl)
     .then(response => response.json())
-    .then(console.log)
+    .then(spiceData => {
+        spiceData.forEach(spice => renderSpices(spice))
+    })
+}
+
+function renderSpices(spice){
+    const spiceImg = document.createElement('img')
+    spiceImg.dataset.id= spice.id 
+    spiceImg.className = 'spice-img'
+    spiceImg.src = spice.image
+
+    spiceMenu.append(spiceImg)
+    
+}
+
+function getSpiceInfo(e){
+    const ImgId = e.target.dataset.id 
+    
+    fetch(`${fetchUrl}/${ImgId}`)
+    .then(response => response.json())
+    .then(spiceDetails => displayDetails(spiceDetails))
+}
+
+function displayDetails(spice){
+    const img = spiceDetails.querySelector('img')
+    img.src = spice.image
 }
 
 getSpice()
