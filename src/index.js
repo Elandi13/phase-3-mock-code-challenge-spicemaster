5fcae62275968dafc1ed0362614d5c8e65edb3d8
Elvis Landi <elandi13@yahoo.com>
2021-02-10 10:59:56 -0500

    step 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+
+const fetchUrl = "http://localhost:3000/spiceblends/1"
+
+const spiceMenu = document.querySelector('#spice-blend-detail')
+
+document.addEventListener('load', getSpice)
+// console.log(spiceMenu)
+
+function getSpice(){
+    fetch(fetchUrl)
+    .then(response => response.json())
+    .then(console.log)
+}
+
+getSpice()
+
